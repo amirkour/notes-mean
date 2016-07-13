26b997004123f0747cdb7bdb8a660343f2c892e7
Amir Kouretchian <amir.kouretchian@gmail.com>
2016-07-13 11:51:56 -0700

    integrating with notes-mysql, fetching and creating tags to/from db as a service, and angular/ui work to integrate w/ service

--- index.js
+++ index.js
@@ -15,9 +15,12 @@ var Promise = require('es6-promise').Promise,
     // this has to be the fully-qualified path to a folder, typically "/templates",
     // where view templates will be found
     absolutePathToViewFolder = path.join(__dirname, 'views'),
+
+    db = require('./notes-mysql/models/index.js'),
     app = express();
     
 
+app.use(bodyparser.json()); // for parsing application/json
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(express.static(absolutePathToPublicAssets));
 app.use(stylus.middleware(absolutePathToPublicAssets));
@@ -25,13 +28,32 @@ app.set('views', absolutePathToViewFolder);
 app.set('view engine', 'jade');
 
 app.get("/api/tags",function(req,res){
-    var tags = [
-        {id:1, name:'bogus one'},
-        {id:2, name:'bogus two'}
-    ];
 
-    console.log("Sending back tags:\n" + JSON.stringify(tags) + "\n");
-    res.json(tags);
+    db.Tags.findAll().then(function(tags){
+        console.log("Sending back tags:\n" + JSON.stringify(tags) + "\n");
+        res.json(tags);
+    }).catch(function(err){
+        console.log("got an error finding all tags: " + err);
+        res.status(500).json(err);
+    });
+
+});
+app.post("/api/tags",function(req,res){
+    console.log("creating new tag");
+
+    if(!req.body || !req.body.name){
+        res.status(400).json({error: 'name required'});
+        return;
+    }
+
+    // TODO - model validation errors?
+    db.Tags.create({namasdfafae: req.body.name}).then(function(obj){
+        console.log("successfully created new tag: " + obj);
+        res.json(obj);
+    }).catch(function(err){
+        console.log("error creating new tag: " + err);
+        res.status(500).json(err);
+    });
 });
 
 app.get("/", function(req,res){
--- src/js/tags/tag-list.component.js
+++ src/js/tags/tag-list.component.js
@@ -1,19 +1,73 @@
-// Register `phoneList` component, along with its associated controller and template
-angular.
-module('tagList').
-component('tagList', {
-  templateUrl: "/compiled/templates/tags/tag-list.template.html",
-  controller: [
-    '$http',
-    function TagListController($http) {
-      var self = this;
+(function($,angular){
+  var http;
 
-      $http.get('/api/tags').then(function(response) {
+  function TagListController($http){
+    http = $http;
+    this.url = '/api/tags';
+    this.error = null;
+
+    this.fetch();
+  }
+
+  $.extend(TagListController.prototype, {
+    onError:function(err){
+      if(!err){
+        this.error = "an unknown error occurred";
+        return;
+      }
+
+      console.log("showing error: " + JSON.stringify(err));
+      if(typeof err === 'string')
+        this.error = err;
+      else if(typeof err.error === 'string')
+        this.error = err.error
+      else
+        this.error = 'failed to parse error';
+    },
+    clearErrors:function(){
+      this.error = null;
+    },
+    fetch: function(){
+      var self = this;
+      this.clearErrors();
+      http.get(this.url).then(function(response){
         self.tags = response.data;
       }).catch(function(err){
-        console.log("err fetching tags: " + err);
+        console.log("error fetching tags from " + self.url + ": " + err);
+        self.onError(err);
       });
+    },
+    create:function(){
+      var self = this;
+
+      if(!this.newName){
+        this.onError("name field required");
+        return;
+      }
+
+      this.clearErrors();
+      http.post(this.url, {name: this.newName}).then(function(response){
+        console.log("Got response: " + JSON.stringify(response.data));
+        self.tags.push(response.data);
+        self.newName = "";
+      }).catch(function(response){
+        console.log("Got err: " + response.data);
+        self.onError(response.data);
+      });
+    },
+    onNewTagClick:function(e){
+      this.create();
+    },
+    onNewTagKeypress:function(e){
+      this.clearErrors();
+      if(e.which === 13){
+        this.create();
+      }
     }
-  ]
-});
-  
+  });
+
+  angular.module('tagList').component('tagList', {
+    templateUrl: "/compiled/templates/tags/tag-list.template.html",
+    controller: ['$http', TagListController]
+  });
+})($,angular);
